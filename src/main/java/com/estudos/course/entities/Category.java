932c0146567e3baf2edfafda4f583fd34244b52d
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-12 22:15:28 -0300

    Revert "Many-to-many association with JoinTable"
    This reverts commit 48f86d7beaaaec0f9e00d9c03e464c4154ac5f71.

--- src/main/java/com/estudos/course/entities/Category.java
+++ src/main/java/com/estudos/course/entities/Category.java
@@ -1,6 +1,5 @@
 package com.estudos.course.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,8 +18,7 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @JsonIgnore
-    @ManyToMany(mappedBy = "categories")
+    @Transient
     private Set<Product> products = new HashSet<>();
 
     public Category() {
--- src/main/java/com/estudos/course/entities/Product.java
+++ src/main/java/com/estudos/course/entities/Product.java
@@ -21,10 +21,7 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @ManyToMany
-    @JoinTable(name = "tb_product_category",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @Transient
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
